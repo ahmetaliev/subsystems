7076d271e827bfaeb030f0172882fce647056ba2
ahmetiliev <officialahmetofficial@gmail.com>
2019-12-08 13:30:08 +0300

    update control panel program

--- control_ini.cpp
+++ control_ini.cpp
@@ -1,1 +1,27 @@
  
+/* Simple test program
+ *
+ *  gcc -o test test.c minIni.c
+ */
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include "minIni.c"
+#define sizearray(a)  (sizeof(a) / sizeof((a)[0]))
+
+const char inifile[] = "test.ini";
+const char inifile2[] = "testplain.ini";
+
+
+int main(){
+    
+    
+a:
+
+ /* string writing */
+   ini_puts("SECTION", "dataid", "data_stored", inifile);
+
+
+//goto a;
+    
+};
